e54422e1d8bac501ce25478bea02dbbecca55a8d
Michel Alves <malvesdesign@hotmail.com>
2023-01-02 13:48:22 -0300

    componente content criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "BATMAN 2022",
+        "description": "Novo Batman é interpretado por Robert Pattinson.",
+        "photoCover": "https://www.proibidoler.com/wp-content/uploads/2022/02/critica-the-batman-2022-4.jpeg"
+    },
+    {
+        "id": "2",
+        "title": "Quem será o novo Super-Homem?",
+        "description": "Henry Cavill não interpretará mais o Super-Homem.",
+        "photoCover": "https://s2.glbimg.com/HyqPTt1R2C-tG4Qmad4S8ybEo-0=/e.glbimg.com/og/ed/f/original/2018/09/12/180912-henry-cavill-superman-feature.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Novo filme do Aquaman será lançado em breve.",
+        "description": "Novo filme do Aquaman tem previsão de lançamento para março de 2023.",
+        "photoCover": "https://i.uai.com.br/lgjyxKa8vUyjA4zp3wOjiiscV08=/750x0/imgsapp2.uai.com.br/app/noticia_133890394703/2022/08/25/298387/jason-momoa-como-aquaman_1_58421.jpeg"
+    },
+    {
+        "id": "4",
+        "title": "The Flash tem lançamento antecipado.",
+        "description": "Após o sucesso dos filmes “Batman” e “Adão Negro” em 2022, que lucraram bem nas bilheterias globais, a DC busca ainda mais em 2023. “The Flash” é a grande esperança da DC para maiores ganhos.",
+        "photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2022/04/ezra-miller-preso.jpg"
+    }
+];
